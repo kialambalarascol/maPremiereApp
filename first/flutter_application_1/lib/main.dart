90d17c34666bbfc0d6b7c5135f87b8eca8f98c88
kialambalarascol <kiala.mbala@e.rascol.net>
2025-10-07 15:02:57 +0200

    utilisation stack et gestureDetector

--- first/flutter_application_1/lib/main.dart
+++ first/flutter_application_1/lib/main.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-  //liste 
-  List<int> notes=[100,8,9];
-  //set pas de doublon, pas ordonné
-  Set<String> color={"blue","red","yellow"};
-  Map<String, int> pi={
-    "john":40,
-    "rwan":18
+
+  // liste 
+  List<int> notes = [100, 8, 9];
+  // set pas de doublon, pas ordonné
+  Set<String> color = {"blue", "red", "yellow"};
+  Map<String, int> pi = {
+    "john": 40,
+    "rwan": 18,
   };
   pi.forEach((key, value) {
-    print ("$key : $value");
-    },);
-
+    print("$key : $value");
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -21,50 +21,51 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'lesson1',
       theme: ThemeData(primarySwatch: Colors.blue),
-
       home: Scaffold(
-        appBar: AppBar(title: Text("Apprentissage",textAlign: TextAlign.center,),
-        backgroundColor: Colors.blueAccent,
-        leading: Icon(Icons.menu),
-        actions: [IconButton(onPressed:(){}, icon: Icon(Icons.favorite))],
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Apprentissage"),
+          backgroundColor: Colors.blueAccent,
+          leading: const Icon(Icons.menu),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.favorite))
+          ],
         ),
-
-      backgroundColor:Color.fromARGB(255, 38, 255, 208),
-      body:GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(20,(index)){
-          return Container(
-            margin:EdgeInsets.all(20),
-            color: Colors.blue.shade300),
-            child: Center(
-                child: Text($index+1),
+        backgroundColor: const Color.fromARGB(255, 38, 255, 208),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () => print("container cliqué"),
+              child: Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      color: Colors.blue.shade300,
+                      width: 200,
+                      height: 200,
+                      child: Center(
+                        child: Text(
+                          "je suis le lutin gronon",
+                          style: TextStyle(
+                            color: Colors.amber.shade700,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-        },
+          ],
+        ),
       ),
-      
-      ),
-        
-          );
+    );
   }
 }
-/*
-body:ListView.builder(itemCount: 50,itemBuilder: (context,index){
-        return ListTile(
-
-                    leading: Icon(Icons.person,color:Colors.green),
-                    title: Text("sujet ${index+1}"),
-                    subtitle: Text("Voici une info pour la ${index+1} personne." ),
-                    subtitleTextStyle: TextStyle(color: Colors.purple),
-                    trailing: Icon(Icons.arrow_forward),
-                    onTap: (){
-
-                    },
-        );
-      },
-          ),
-*/
